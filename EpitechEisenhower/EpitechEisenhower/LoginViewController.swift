c26af2d51e908579de6821fa3ad4c3de78ca5737
fauquette fred <fredfocmac@gmail.com>
2017-09-25 16:46:12 +0200

    add assets, create loginVC, create homeVC, create connect button

--- EpitechEisenhower/EpitechEisenhower/LoginViewController.swift
+++ EpitechEisenhower/EpitechEisenhower/LoginViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    @IBOutlet weak var connectButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Login"
+        connectButton.layer.cornerRadius = 5
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func connect(_ sender: Any) {
+        performSegue(withIdentifier: "showHome", sender: nil)
+    }
+    
 }
 
